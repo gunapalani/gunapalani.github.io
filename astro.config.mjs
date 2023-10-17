7de43541d796a14328941ce16e997e87e3cedab4
gunalogesh <144795061+gunalogesh@users.noreply.github.com>
2023-10-17 10:20:57 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+// import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: "https://gunalogesh.github.io",
+  integrations: [tailwind(), react()],
 });
