02a93827594c74c32a156a2a54ad7928c61d66f4
Gunasekaran palani <92242069+gunapalani@users.noreply.github.com>
2023-10-29 19:28:37 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://gunalogesh.github.io",
+  site: "https://gunapalani.github.io",
   integrations: [tailwind(), react()],
 });
