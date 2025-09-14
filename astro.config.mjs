34fa2ecce9ad965ff5323e0f0eba8aeae3cea4c9
gunapalani <gunapalani19@outlook.com>
2025-09-14 17:52:52 +0530

    feat: revamped new website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-// import node from "@astrojs/node";
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://gunapalani.github.io",
-  integrations: [tailwind(), react()],
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
